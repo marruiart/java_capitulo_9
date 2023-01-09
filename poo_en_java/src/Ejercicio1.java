234a67f078622299b6b5ab4750428e17c43042f0
Marina Ruiz <88201067+marruiart@users.noreply.github.com>
2023-01-09 20:44:47 +0100

    update Ejercicio1

--- poo_en_java/src/Ejercicio1.java
+++ poo_en_java/src/Ejercicio1.java
@@ -1,5 +1,24 @@
-public class Main {
+/**
+ * Implementa la clase Caballo con los atributos y métodos que pueda tener. Pruébala creando
+ * instancias y aplicándole algunos métodos.
+ *
+ * @author: Marina Ruiz Artacho
+ **/
+
+import Animales.Caballo;
+
+public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Caballo perdigon = new Caballo("Perdigón", "Jaspeado", "Appaloosa", Caballo.Sexo.MACHO, 7, 420);
+
+        if (perdigon.velocidad == 0)
+            System.out.printf("%s está parado a %d km/h\n", perdigon.nombre, perdigon.velocidad);
+        perdigon.alPaso();
+        System.out.printf("%s va al paso a %d km/h\n", perdigon.nombre, perdigon.velocidad);
+        perdigon.alTrote();
+        System.out.printf("%s va al trote a %d km/h\n", perdigon.nombre, perdigon.velocidad);
+        perdigon.relincha();
+        String sexo = perdigon.sexo.equals(Caballo.Sexo.MACHO) ? "caballo" : "yegua";
+        System.out.printf("%s es un%s %s", perdigon.nombre, sexo.equals("caballo") ? "" : "a", sexo);
     }
 }
