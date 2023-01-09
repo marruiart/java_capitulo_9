392fe9836ba3396db00664eee6566ef60c36bd23
Marina Ruiz <marruiart@gmail.com>
2023-01-09 21:25:32 +0100

    update Ejercicio1

--- poo_en_java/src/Ejercicio1.java
+++ poo_en_java/src/Ejercicio1.java
@@ -9,13 +9,13 @@ import Animales.Caballo;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        Caballo perdigon = new Caballo("Perdigón", "Jaspeado", "Appaloosa", Caballo.Sexo.MACHO, 7, 420);
+        Caballo perdigon = new Caballo("Perdigón", "Jaspeado", "Appaloosa", Caballo.Sexo.MACHO, "10-01-2017", 420);
 
         if (perdigon.velocidad == 0)
             System.out.printf("%s está parado a %d km/h\n", perdigon.nombre, perdigon.velocidad);
-        perdigon.alPaso();
+        perdigon.irAlPaso();
         System.out.printf("%s va al paso a %d km/h\n", perdigon.nombre, perdigon.velocidad);
-        perdigon.alTrote();
+        perdigon.irAlTrote();
         System.out.printf("%s va al trote a %d km/h\n", perdigon.nombre, perdigon.velocidad);
         perdigon.relincha();
         String sexo = perdigon.sexo.equals(Caballo.Sexo.MACHO) ? "caballo" : "yegua";
